074c08fda4698a7d850f5eeeaaf5ee70698a4542
braden <26287414+bradenschiller@users.noreply.github.com>
2022-07-16 14:40:02 -0500

    remove boiler plate code from create react app

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./App.css";
 import styled from "styled-components";
 import OperationBlock from "./OperationBlock";
 import { OperationTypes } from "./Constants";
